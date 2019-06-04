773c4114e6f0d6c827eaa70afeaefd0a4cc9f68c
asy0239 <asy0239@gmail.com>
2019-06-04 17:32:43 +0900

    django-db | 190604

--- 4_django/FIRST_DJANGO/utils/urls.py
+++ 4_django/FIRST_DJANGO/utils/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),   # utils/
-    path('art/<str:keyword>/', views.artii),   # utils/art/<keyword>
-    path('stock/', views.stock),  # utils/stock/
+    path('', views.index),  # utils/
+    path('art/<str:keyword>/', views.artii),  # utils/art/<KEYWORD>
+    path('stock_input/', views.stock_input),  # utils/stock_input/
+    path('stock_output/', views.stock_output),  # utils/stock_output/
 ]
--- 4_django/FIRST_DJANGO/utils/views.py
+++ 4_django/FIRST_DJANGO/utils/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-import art
 
-# Create your views here.
+from django.shortcuts import render
 
 
 def index(request):
@@ -9,7 +7,8 @@ def index(request):
 
 
 def artii(request, keyword):
-    result = art.text2art(keyword, font='alpha')
+    import art
+    result = art.text2art(keyword, 'ghost')
     context = {
         'result': result,
         'keyword': keyword,
@@ -17,5 +16,27 @@ def artii(request, keyword):
     return render(request, 'utils/artii.html', context)
 
 
-def stock(request):
-    pass  # todo: 완성하기
+# 사용자 입력 form 을 제공하는 액션
+def stock_input(request):
+    return render(request, 'utils/stock_input.html')
+
+
+# 입력받은 data 를 처리하여 결과를 제공하는 액션
+def stock_output(request):
+    from iexfinance.stocks import Stock
+
+    company_code = request.GET.get('company_code')
+    TOKEN = 'pk_34947d6f20564c52a9dcd62bf4d4ab5f'
+
+    try:
+        stock = Stock(company_code, token=TOKEN)
+        data = stock.get_quote()
+    except:
+        return render(request, 'utils/stock_output.html', {
+            'is_ok': False,
+            'message': '검색할 수 없습니다.'
+        })
+    return render(request, 'utils/stock_output.html', {
+        'is_ok': True,
+        'data': data
+    })
